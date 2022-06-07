14cc3a936224f2ceab071ddd2a8cc2734e8d8e2b
FlutLab.io <>
2022-06-07 13:04:52 +0000

    Autosave

--- lib/drag/bowlingOrder.dart
+++ lib/drag/bowlingOrder.dart
@@ -42,13 +42,13 @@ class _BowlingOrderPageState extends State<BowlingOrderPage> {
           children: <Widget>[
             CircleAvatar(
               backgroundColor: Colors.green,
-              radius: 55,
+              radius: 40,
               child: CircleAvatar(
                 backgroundColor: Colors.greenAccent[100],
-                radius: 110,
+                radius: 35,
                 child: CircleAvatar(
                   backgroundImage: NetworkImage("https://pbs.twimg.com/profile_images/1304985167476523008/QNHrwL2q_400x400.jpg"), //NetworkImage
-                  radius: 50,
+                  radius: 30,
                 ), //CircleAvatar
               ), //CircleAvatar
             ),
