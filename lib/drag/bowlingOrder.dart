82117995c3e0ee885b7591e042b24c31a02fdddb
FlutLab.io <>
2022-06-07 11:44:47 +0000

    Autosave

--- lib/drag/bowlingOrder.dart
+++ lib/drag/bowlingOrder.dart
@@ -65,7 +65,7 @@ class _BowlingOrderPageState extends State<BowlingOrderPage> {
     if (widget.allPlayers != null) {
       players = widget.allPlayers;
     }
-    print(players[0]);
+    print(json.decode(json.encode(players)));
 
     // if(widget.allPlayers!=null){
     //   widget.allPlayers.map((e)=>(data.add(_DragablePlayer("1"))));
