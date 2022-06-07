c1f56d83ccce3b44e6b051cd5a972886b4b1df29
FlutLab.io <>
2022-06-07 12:28:36 +0000

    Autosave

--- lib/drag/bowlingOrder.dart
+++ lib/drag/bowlingOrder.dart
@@ -35,6 +35,7 @@ List<String> todos = [
 class _BowlingOrderPageState extends State<BowlingOrderPage> {
   Widget _DragablePlayer(Object data) {
     Map<String, dynamic>? PlayerData = json.decode(json.encode(data));
+    print(PlayerData?["name"]);
     return Container(height: 100, child: Text(PlayerData?['name']));
   }
 
