0616f880441c614c6b62285005d6ac327260253c
FlutLab.io <>
2022-06-07 13:43:57 +0000

    Autosave

--- lib/drag/bowlingOrder.dart
+++ lib/drag/bowlingOrder.dart
@@ -61,7 +61,7 @@ class _BowlingOrderPageState extends State<BowlingOrderPage> {
             Column(children: [
               Text(PlayerData?["name"]),
               Text(PlayerData?["nationality"]),
-              PlayerData?["additional_skill"]?Text(PlayerData?["additional_skill"]):,
+              PlayerData?["additional_skill"] ? Text(PlayerData?["additional_skill"]) : Text(""),
               // Text(PlayerData?["bowling"]["style"]),
             ])
           ],
