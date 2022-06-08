cb650be9e9b92b036011705be185ce74d22da530
FlutLab.io <>
2022-06-08 08:43:48 +0000

    Autosave

--- lib/drag/bowlingOrder.dart
+++ lib/drag/bowlingOrder.dart
@@ -186,9 +186,9 @@ class _BowlingOrderPageState extends State<BowlingOrderPage> {
   Widget build(BuildContext context) {
     return Container(
         child: Stack(children: <Widget>[
-      new ListView(
-        children: todos.map((todo) => new ListTile(title: new Text(todo))).toList(),
-      ),
+      // new ListView(
+      //   children: todos.map((todo) => new ListTile(title: new Text(todo))).toList(),
+      // ),
       _OversToBeBowled(),
       _allPlayersComponenet(),
     ]));
