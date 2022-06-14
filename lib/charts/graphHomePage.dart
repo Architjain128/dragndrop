eed2e5c6eaf31289a0884b5d18f16cdb4aee696a
FlutLab.io <>
2022-06-14 09:01:13 +0000

    Autosave

--- lib/charts/graphHomePage.dart
+++ lib/charts/graphHomePage.dart
@@ -2,13 +2,11 @@ import 'package:dragAndDrop/main.dart';
 import 'package:flutter/material.dart';
 import '../data.dart';
 import 'dart:core';
+import "./combinedGraphs.dart"
 
 class GraphHomePage extends StatefulWidget {
   final PandoraBox model;
-  // final List<Page> _pages = [
-  //   Page("team1", Icons.person_outline),
-  //   Page("team2", Icons.person_outline),
-  // ];
+
   GraphHomePage({Key? key, required this.model}) : super(key: key);
   @override
   _GraphHomePageState createState() => _GraphHomePageState();
@@ -34,7 +32,7 @@ class _GraphHomePageState extends State<GraphHomePage> {
         title: Text("Graphs"),
       ),
       body: Center(
-        child: Text("HI"),
+        child: ChartApp(),
       ),
     );
   }
