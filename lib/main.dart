4588d98c3d7592bb09c94d305cd2446bb59f9e33
FlutLab.io <>
2022-06-14 14:10:41 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -42,7 +42,7 @@ class MyApp extends StatelessWidget {
                 },
               ),
         },
-        initialRoute: '/graph',
+        initialRoute: '/',
         // home: MyHomePage(),
         // home: ScopedModelDescendant<PandoraBox>(
         //   builder: (context, child, model) {
