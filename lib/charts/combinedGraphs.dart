a99b853c99172bb5002ba3a333af68d1aec984f3
FlutLab.io <>
2022-06-14 12:22:27 +0000

    Autosave

--- lib/charts/combinedGraphs.dart
+++ lib/charts/combinedGraphs.dart
@@ -49,20 +49,6 @@ class _CombinedChartPageState extends State<CombinedChartPage> {
         100,
         80,
         62
-      ], [
-        "bar",
-        "bar",
-        "bar",
-        "line",
-        "line",
-        "line"
-      ], [
-        "b1",
-        "b2",
-        "b3",
-        "l1",
-        "l2",
-        "l3"
       ]),
       _ChartData("kohli", [
         11,
@@ -71,20 +57,6 @@ class _CombinedChartPageState extends State<CombinedChartPage> {
         95,
         81,
         60
-      ], [
-        "bar",
-        "bar",
-        "bar",
-        "line",
-        "line",
-        "line"
-      ], [
-        "b1",
-        "b2",
-        "b3",
-        "l1",
-        "l2",
-        "l3"
       ]),
       _ChartData("Rohit", [
         10,
@@ -93,20 +65,6 @@ class _CombinedChartPageState extends State<CombinedChartPage> {
         100,
         80,
         69
-      ], [
-        "bar",
-        "bar",
-        "bar",
-        "line",
-        "line",
-        "line"
-      ], [
-        "b1",
-        "b2",
-        "b3",
-        "l1",
-        "l2",
-        "l3"
       ]),
       _ChartData("Dhawan", [
         11,
@@ -115,20 +73,6 @@ class _CombinedChartPageState extends State<CombinedChartPage> {
         90,
         84,
         60
-      ], [
-        "bar",
-        "bar",
-        "bar",
-        "line",
-        "line",
-        "line"
-      ], [
-        "b1",
-        "b2",
-        "b3",
-        "l1",
-        "l2",
-        "l3"
       ]),
       _ChartData("Rahane", [
         10,
@@ -137,20 +81,22 @@ class _CombinedChartPageState extends State<CombinedChartPage> {
         100,
         80,
         63
-      ], [
-        "bar",
-        "bar",
-        "bar",
-        "line",
-        "line",
-        "line"
-      ], [
-        "b1",
-        "b2",
-        "b3",
-        "l1",
-        "l2",
-        "l3"
+      ]),
+      _ChartData("KL", [
+        11,
+        20,
+        14,
+        90,
+        84,
+        60
+      ]),
+      _ChartData("Ashwin", [
+        10,
+        21,
+        13,
+        100,
+        80,
+        63
       ]),
     ];
     super.initState();
@@ -165,7 +111,7 @@ class _CombinedChartPageState extends State<CombinedChartPage> {
   SfCartesianChart _buildDefaultLineChart() {
     return SfCartesianChart(
       plotAreaBorderWidth: 0,
-      title: ChartTitle(text: 'Inflation - Consumer price'),
+      title: ChartTitle(text: 'Graphical View'),
       legend: Legend(isVisible: true, overflowMode: LegendItemOverflowMode.wrap),
       primaryXAxis: CategoryAxis(),
       // primaryXAxis: CategoryAxis(edgeLabelPlacement: EdgeLabelPlacement.shift, majorGridLines: const MajorGridLines(width: 0)),
@@ -185,8 +131,8 @@ class _CombinedChartPageState extends State<CombinedChartPage> {
             width: 0.8,
             spacing: 0.1,
             dataSource: data,
-            xValueMapper: (_ChartData sales, _) => sales.playerName,
-            yValueMapper: (_ChartData sales, _) => sales.values[i],
+            xValueMapper: (_ChartData row, _) => row.playerName,
+            yValueMapper: (_ChartData row, _) => row.values[i],
             name: categoryName[i],
             animationDuration: 2500,
           ),
@@ -197,8 +143,8 @@ class _CombinedChartPageState extends State<CombinedChartPage> {
           LineSeries<_ChartData, String>(
             animationDuration: 2500,
             dataSource: data,
-            xValueMapper: (_ChartData sales, _) => sales.playerName,
-            yValueMapper: (_ChartData sales, _) => sales.values[i],
+            xValueMapper: (_ChartData row, _) => row.playerName,
+            yValueMapper: (_ChartData row, _) => row.values[i],
             width: 2,
             name: categoryName[i],
             markerSettings: const MarkerSettings(isVisible: true),
@@ -207,76 +153,11 @@ class _CombinedChartPageState extends State<CombinedChartPage> {
       }
     }
     return chartAll;
-    // return <LineSeries<_ChartData, String>>[
-    //   LineSeries<_ChartData, String>(
-    //     animationDuration: 2500,
-    //     dataSource: data,
-    //     xValueMapper: (_ChartData sales, _) => sales.playerName,
-    //     yValueMapper: (_ChartData sales, _) => sales.values[0],
-    //     width: 2,
-    //     name: categoryName[i],
-    //     markerSettings: const MarkerSettings(isVisible: true),
-    //   ),
-    //   LineSeries<_ChartData, String>(
-    //     animationDuration: 2500,
-    //     dataSource: data,
-    //     width: 2,
-    //     name: 'Jain',
-    //     xValueMapper: (_ChartData sales, _) => sales.playerName,
-    //     yValueMapper: (_ChartData sales, _) => sales.values[1],
-    //     markerSettings: const MarkerSettings(isVisible: true),
-    //   ),
-    // ];
   }
-
-  // @override
-  // Widget build(BuildContext context) {
-  //   return Column(children: [
-  //     //Initialize the chart widget
-  //     SfCartesianChart(
-  //         primaryXAxis: CategoryAxis(),
-  //         // Chart title
-  //         title: ChartTitle(text: 'temp'),
-  //         // Enable legend
-  //         legend: Legend(isVisible: true),
-  //         // Enable tooltip
-  //         tooltipBehavior: TooltipBehavior(enable: true),
-  //         series: <ChartSeries<_ChartData, String>>[
-  //           LineSeries<_ChartData, String>(
-  //             dataSource: data,
-  //             xValueMapper: (_ChartData sales, _) => sales.lineName[0],
-  //             yValueMapper: (_ChartData sales, _) => sales.line[0],
-  //             name: 'a',
-  //             // Enable data label
-  //             dataLabelSettings: DataLabelSettings(isVisible: true),
-  //           )
-  //         ]),
-
-  // Expanded(
-  //   child: Padding(
-  //     padding: const EdgeInsets.all(8.0),
-  //     //Initialize the spark charts widget
-  //     child: SfSparkLineChart.custom(
-  //       //Enable the trackball
-  //       trackball: SparkChartTrackball(activationMode: SparkChartActivationMode.tap),
-  //       //Enable marker
-  //       marker: SparkChartMarker(displayMode: SparkChartMarkerDisplayMode.all),
-  //       //Enable data label
-  //       labelDisplayMode: SparkChartLabelDisplayMode.all,
-  //       xValueMapper: (int index) => data[index].year,
-  //       yValueMapper: (int index) => data[index].sales,
-  //       dataCount: 5,
-  //     ),
-  //   ),
-  // )
-  // ]);
-  // }
 }
 
 class _ChartData {
-  _ChartData(this.playerName, this.values, this.category, this.categoryName);
+  _ChartData(this.playerName, this.values);
   final String? playerName;
   final List<double> values;
-  final List<String> category;
-  final List<String> categoryName;
 }
